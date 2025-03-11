54b4dc856048a509a8c092dd3cda4755c83be6e0
srnyx <25808801+srnyx@users.noreply.github.com>
2025-03-11 17:02:18 -0400

    Update Annoying API
    5.1.5 -> f7afe0fba1

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
     id("com.gradleup.shadow") version "8.3.6"
 }
 
-setupAnnoyingAPI("5.1.5", "xyz.srnyx", "2.1.0", "Plugin used for per-player phantom spawning/control")
+setupAnnoyingAPI("f7afe0fba1", "xyz.srnyx", "2.1.0", "Plugin used for per-player phantom spawning/control")
 spigotAPI("1.13")
 
 repository(Repository.PLACEHOLDER_API)

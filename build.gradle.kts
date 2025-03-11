b6edc6a8bfeca9a48aa3e8f860125d8926e74fa3
srnyx <25808801+srnyx@users.noreply.github.com>
2025-03-11 17:01:12 -0400

    Update Gradle & plugins
    Gradle: 8.11.1 -> 8.13
    Gradle Galaxy: 1.3.2 -> 1.3.3
    Shadow: 8.3.5 -> 8.3.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ import xyz.srnyx.gradlegalaxy.utility.spigotAPI
 
 plugins {
     java
-    id("xyz.srnyx.gradle-galaxy") version "1.3.2"
-    id("com.gradleup.shadow") version "8.3.5"
+    id("xyz.srnyx.gradle-galaxy") version "1.3.3"
+    id("com.gradleup.shadow") version "8.3.6"
 }
 
 setupAnnoyingAPI("5.1.5", "xyz.srnyx", "2.1.0", "Plugin used for per-player phantom spawning/control")

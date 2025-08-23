fb4ed808b32b9af3df1c85a97a3d22bad5883777
srnyx <25808801+srnyx@users.noreply.github.com>
2025-08-23 16:03:07 -0400

    Update dependencies
    Gradle: 8.13 -> 9.0.0
    Shadow: 8.3.6 -> 8.3.8
    Annoying API: 0d3c33c14c -> 501e138120

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,11 +7,11 @@ import xyz.srnyx.gradlegalaxy.utility.spigotAPI
 plugins {
     java
     id("xyz.srnyx.gradle-galaxy") version "1.3.3"
-    id("com.gradleup.shadow") version "8.3.6"
+    id("com.gradleup.shadow") version "8.3.8"
 }
 
-setupAnnoyingAPI("0d3c33c14c", "xyz.srnyx", "2.1.2", "Plugin used for per-player phantom spawning/control")
 spigotAPI("1.13")
+setupAnnoyingAPI("501e138120", "xyz.srnyx", "2.1.2", "Plugin used for per-player phantom spawning/control", JavaVersion.VERSION_1_8)
 
 repository(Repository.PLACEHOLDER_API)
 dependencies.compileOnly("me.clip", "placeholderapi", "2.11.6")
